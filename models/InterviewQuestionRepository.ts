e4a8a3919b99fee9680ae16c6581f0c9762ca3e4
wilsonplau <hello@wilsonplau.com>
2021-06-05 20:34:41 +0900

    feat: load interview questions from Markdown

---
+++ models/InterviewQuestionRepository.ts
@@ -0,0 +1,41 @@
+import { PrismaClient, QuestionType } from ".prisma/client";
+import { QuestionDifficulty } from "@prisma/client";
+import matter from "gray-matter";
+
+export default class InterviewQuestionRepoistory {
+  prisma: PrismaClient;
+
+  constructor(prismaClient: PrismaClient) {
+    this.prisma = prismaClient;
+  }
+
+  async createOrUpdateFromMarkdown(id: string, questionMd: string, answerMd: string) {
+    const questionParsed = matter(questionMd);
+    const answerParsed = matter(answerMd);
+
+    // TODO: Get maps from DB
+    const difficultyMap: { [key: string]: QuestionDifficulty } = {
+      Easy: QuestionDifficulty.EASY,
+      Medium: QuestionDifficulty.MEDIUM,
+      Hard: QuestionDifficulty.HARD,
+    };
+
+    const typeMap: { [key: string]: QuestionType } = {
+      Array: QuestionType.ARRAY,
+      LinkedList: QuestionType.LINKED_LIST,
+    };
+
+    const questionName = questionParsed.data.name as string;
+    const questionDifficulty = difficultyMap[questionParsed.data.difficulty];
+    const questionText = questionParsed.content as string;
+    const questionType = questionParsed.data.types.map((type: string) => typeMap[type]);
+    const answerText = answerParsed.content;
+
+    const interviewQuestion = await this.prisma.interviewQuestion.upsert({
+      where: { id },
+      update: { questionName, questionText, questionDifficulty, questionType, answerText },
+      create: { id, questionName, questionText, questionDifficulty, questionType, answerText },
+    });
+    console.log(interviewQuestion);
+  }
+}
