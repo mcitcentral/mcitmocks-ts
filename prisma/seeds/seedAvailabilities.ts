3918e3db5899873f2e4ec12bacc7edc2ba6a65d9
Xinyi Samore <xinyisamore@phs006611.lan>
2021-06-18 16:30:21 -0500

    clean code in seedAvailabilities.ts

--- prisma/seeds/seedAvailabilities.ts
+++ prisma/seeds/seedAvailabilities.ts
@@ -1,32 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import AvailabilityRepository from "../../models/AvailabilityRepository";
 
-//// Instantiate Prisma Client
-
-/*export default async function main(){
-    //just for testing purpose
-    await prisma.availability.deleteMany({})
-
-
-    const availability= await prisma.availability.create({
-        data:[{
-           
-                    id:'11',
-                    userId: '1',
-                    startTime:new Date(2021,6,20,10,30,30,80),
-                    isTaken: true
-                },
-                {
-                    id:'12',
-                    userId: '2',
-                    startTime:new Date(2021,6,25,11,30,30,80),
-                    isTaken: true
-                },],
-
-        /*include: {
-            availability: true,
-        },
-    })*/
 
 export default async function main(prisma: PrismaClient) {
   const availability1 = await prisma.availability.upsert({
