1ea83cfda6f7d477e8c58edf0b142d779acb20b6
Xinyi Samore <xinyisamore@phs006611.lan>
2021-06-29 23:28:13 -0500

    updated availabilityController and index.ts

--- controllers/availabilityController.ts
+++ controllers/availabilityController.ts
@@ -1,16 +1,15 @@
 import { PrismaClient } from ".prisma/client";
 import express, { Request, Response, Router } from "express";
 import AvailabilityRepository from "../models/AvailabilityRepository";
-import { formatDistance, subDays } from "date-fns";
-import formatISO from "date-fns/formatISO";
-export default function AvailabilityController(prismaClient: PrismaClient) {
+import { formatDistance, subDays, formatISO } from "date-fns";
+export default function availabilityController(prismaClient: PrismaClient) {
   const availibilityRouter = express.Router();
   const availabilityRepository = new AvailabilityRepository(prismaClient);
 
-  availibilityRouter.get("/availibility/:startTime/:endTime", async (req: Request, res: Response) => {
+  availibilityRouter.get("/:startTime/:endTime", async (req: Request, res: Response) => {
     try {
       if (!req.user) return res.status(401).send({ error: "Not Authenticated." });
-      if (req.params.startTime == null && req.params.endTime == null) {
+      if (req.params.startTime == null || req.params.endTime == null) {
         let startTime = subDays(new Date(), 7);
         let endTime = new Date();
         const availability = await availabilityRepository.getAvailability(
@@ -19,8 +18,8 @@ export default function AvailabilityController(prismaClient: PrismaClient) {
           formatISO(endTime)
         );
       }
-      if (Date.parse(req.params.startTime) == NaN || Date.parse(req.params.endTime) == NaN)
-        return res.status(401).send({ error: "startTime or endTime in the wrong format." });
+      if (Number.isNaN(Date.parse(req.params.startTime)) || Number.isNaN(Date.parse(req.params.endTime)))
+        return res.status(400).send({ error: "startTime or endTime in the wrong format." });
       const availability = await availabilityRepository.getAvailability(
         req.user.id,
         req.params.startTime,
@@ -31,4 +30,5 @@ export default function AvailabilityController(prismaClient: PrismaClient) {
       res.status(500).send({ error: e.message });
     }
   });
+  return availibilityRouter;
 }
