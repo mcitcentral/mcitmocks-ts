3b0a57d38bf7b497bd8eb6cc19ee41ac87707ca8
Xinyi Samore <xinyisamore@phs006611.lan>
2021-07-01 23:26:57 -0500

    updated availabilityController.ts

--- controllers/availabilityController.ts
+++ controllers/availabilityController.ts
@@ -6,24 +6,26 @@ export default function availabilityController(prismaClient: PrismaClient) {
   const availibilityRouter = express.Router();
   const availabilityRepository = new AvailabilityRepository(prismaClient);
 
-  availibilityRouter.get("/:startTime/:endTime", async (req: Request, res: Response) => {
+  availibilityRouter.get('/', async(req, res)=> {
     try {
+      let startTime = req.query.startTime;
+      let endTime = req.query.endTime;
       if (!req.user) return res.status(401).send({ error: "Not Authenticated." });
-      if (req.params.startTime == null || req.params.endTime == null) {
-        let startTime = subDays(new Date(), 7);
-        let endTime = new Date();
+      if (startTime == null || endTime == null) {
+        let newStartTime = subDays(new Date(), 7);
+        let newEndTime = new Date();
         const availability = await availabilityRepository.getAvailability(
           req.user.id,
-          formatISO(startTime),
-          formatISO(endTime)
+          formatISO(newStartTime),
+          formatISO(newStartTime)
         );
       }
-      if (Number.isNaN(Date.parse(req.params.startTime)) || Number.isNaN(Date.parse(req.params.endTime)))
+      if (Number.isNaN(Date.parse(String(startTime))) || Number.isNaN(Date.parse(String(endTime))))
         return res.status(400).send({ error: "startTime or endTime in the wrong format." });
       const availability = await availabilityRepository.getAvailability(
         req.user.id,
-        req.params.startTime,
-        req.params.endTime
+        String(startTime),
+        String(endTime)
       );
       res.status(200).json(availability);
     } catch (e) {
