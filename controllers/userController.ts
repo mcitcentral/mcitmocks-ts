b9645b45b04bc65d0286487ca5c030eaeeaf70d6
Josh Hanson <jmhanson@seas.upenn.edu>
2021-07-04 07:15:30 -0400

    fix: Return updated user in userController
    Switched to using POST body rather than URL queries

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -1,32 +1,19 @@
-import { PrismaClient, QuestionDifficulty, QuestionType, CodingLanguage } from ".prisma/client";
+import { PrismaClient, User } from ".prisma/client";
 import express, { Request, Response } from "express";
-import { UpdateUserPreferencesRequest, UserPreferences } from "../@types";
+import { UpdateUserPreferencesRequest } from "../@types";
 import UserRepository from "../models/UserRepository";
 
 export default function userController(prismaClient: PrismaClient) {
     const userRouter = express.Router();
     const userRepository = new UserRepository(prismaClient);
 
-    userRouter.post("/", async (req: Request<{}, {}, UpdateUserPreferencesRequest>, res: Response) => {
+    userRouter.post("/", async (req: Request<{}, {}, Partial<UpdateUserPreferencesRequest>>, res: Response<any, User>) => {
         const userId = req.user?.id;
-        const timeZone = (req.query.timezone as string) || null;
-        const questionTypes = (req.query.questionTypes as QuestionType[]) || null;
-        const questionDifficulty = (req.query.questionDifficulty as QuestionDifficulty) || null;
-        const codingLanguage = (req.query.codingLanguage as CodingLanguage[]) || null;
-
-        let prefs: Partial<UserPreferences> = {};
-        if (timeZone) prefs.timeZone = timeZone;
-        if (questionTypes) prefs.questionTypes = questionTypes;
-        if (questionDifficulty) prefs.questionDifficulty = questionDifficulty;
-        if (codingLanguage) prefs.codingLanguage = codingLanguage;
 
         try {
-            if (!timeZone && !questionTypes && !questionDifficulty && !codingLanguage)
-                return res.status(400).send({ error: "Empty Request." });
-
             if (!userId) return res.status(401).send({ error: "Not Authenticated." });
-            await userRepository.updateUserById(userId, prefs);
-            res.send(200).json(prefs);
+            const user = await userRepository.updateUserById(userId, req.body);
+            res.send(200).json(user);
         } catch (e) {
             res.status(500).send({ error: e.message });
         }
