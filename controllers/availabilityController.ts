9fafee5ba3d2141f41f277bb9745550c7c22cb74
Xinyi Samore <xinyisamore@phs006611.lan>
2021-07-04 08:20:24 -0500

    updated controllers/availabilityController.ts

--- controllers/availabilityController.ts
+++ controllers/availabilityController.ts
@@ -1,25 +1,24 @@
 import { PrismaClient } from ".prisma/client";
-import express, { Request, Response, Router } from "express";
+import express, { Request, Response } from "express";
 import AvailabilityRepository from "../models/AvailabilityRepository";
-import { formatDistance, subDays, formatISO } from "date-fns";
+import { subDays } from "date-fns";
 export default function availabilityController(prismaClient: PrismaClient) {
   const availibilityRouter = express.Router();
   const availabilityRepository = new AvailabilityRepository(prismaClient);
-
-  availibilityRouter.get('/', async(req, res)=> {
+  availibilityRouter.get("/", async (req: Request, res: Response) => {
     try {
-      let startTime = req.query.startTime;
-      let endTime = req.query.endTime;
+      let startTime = req.query.startTime || subDays(new Date(), 7);
+      let endTime = req.query.endTime || new Date();
       if (!req.user) return res.status(401).send({ error: "Not Authenticated." });
-      if (startTime == null || endTime == null) {
-        let newStartTime = subDays(new Date(), 7);
-        let newEndTime = new Date();
+      const newEndTime = new Date();
+      /*if (startTime == null || endTime == null) {
+        const newStartTime = subDays(new Date(), 7);
         const availability = await availabilityRepository.getAvailability(
           req.user.id,
           formatISO(newStartTime),
-          formatISO(newStartTime)
+          formatISO(newEndTime)
         );
-      }
+      }*/
       if (Number.isNaN(Date.parse(String(startTime))) || Number.isNaN(Date.parse(String(endTime))))
         return res.status(400).send({ error: "startTime or endTime in the wrong format." });
       const availability = await availabilityRepository.getAvailability(
