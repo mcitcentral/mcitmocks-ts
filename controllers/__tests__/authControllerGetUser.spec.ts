7f2f0da01c129b22b6502907cab9cd3c827f1145
wilsonplau <hello@wilsonplau.com>
2021-07-10 17:27:07 +0900

    chore: use docker for tests

--- controllers/__tests__/authControllerGetUser.spec.ts
+++ controllers/__tests__/authControllerGetUser.spec.ts
@@ -1,7 +1,6 @@
 import request from "supertest";
 import { PrismaClient } from "@prisma/client";
 
-import app from "../../index";
 import AuthService from "../../services/AuthService";
 
 describe("GET /api/auth/user", () => {
@@ -9,7 +8,7 @@ describe("GET /api/auth/user", () => {
   const authService = new AuthService(prismaClient);
 
   it("should respond with 401 not authorized if cookie not provided", async () => {
-    const res = await request(app).get("/api/auth/user");
+    const res = await request("localhost:8000").get("/api/auth/user");
     expect(res.statusCode).toEqual(401);
     expect(res.body).toEqual({ error: "Not Authenticated." });
   });
@@ -17,7 +16,7 @@ describe("GET /api/auth/user", () => {
   it("should return user if a valid cookie is provided", async () => {
     const user = await prismaClient.user.findUnique({ where: { id: "1" } });
     const token = authService.generateJWT(user!);
-    const res = await request(app).get("/api/auth/user").set("cookie", `mcitmocks=${token}`);
+    const res = await request("localhost:8000").get("/api/auth/user").set("cookie", `mcitmocks=${token}`);
     expect(res.statusCode).toEqual(200);
     expect(res.body).toEqual({
       user: {
