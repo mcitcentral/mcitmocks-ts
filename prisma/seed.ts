2c4ece870edfb678e0f4c655d35c53a349a933cb
wilsonplau <hello@wilsonplau.com>
2021-07-10 19:24:12 +0900

    fix: add try-catch

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,25 +5,29 @@ import seedAvailabilities from "./seeds/seedAvailabilities";
 import seedInterviews from "./seeds/seedInterviews";
 
 export default async function main() {
-  const prisma = new PrismaClient();
+  try {
+    const prisma = new PrismaClient();
 
-  // Delete in reverse order
-  const deleteUsers = prisma.user.deleteMany({});
-  const deleteInterviewQuestions = prisma.interviewQuestion.deleteMany({});
-  const deleteAvailabilities = prisma.availability.deleteMany({});
-  const deleteInterviews = prisma.interview.deleteMany({});
-  await prisma.$transaction([deleteInterviews, deleteAvailabilities, deleteInterviewQuestions, deleteUsers]);
-  console.log("Deleting everything");
+    // Delete in reverse order
+    const deleteUsers = prisma.user.deleteMany({});
+    const deleteInterviewQuestions = prisma.interviewQuestion.deleteMany({});
+    const deleteAvailabilities = prisma.availability.deleteMany({});
+    const deleteInterviews = prisma.interview.deleteMany({});
+    await prisma.$transaction([deleteInterviews, deleteAvailabilities, deleteInterviewQuestions, deleteUsers]);
+    console.log("Deleting everything");
 
-  // Seed in order based on dependencies
-  await seedUsers(prisma);
-  console.log("Seed Users");
-  await seedInterviewQuestions(prisma);
-  console.log("Seed Interview Questions");
-  await seedAvailabilities(prisma);
-  console.log("Seed Availabilities");
-  await seedInterviews(prisma);
-  console.log("Seed Interviews");
+    // Seed in order based on dependencies
+    await seedUsers(prisma);
+    console.log("Seed Users");
+    await seedInterviewQuestions(prisma);
+    console.log("Seed Interview Questions");
+    await seedAvailabilities(prisma);
+    console.log("Seed Availabilities");
+    await seedInterviews(prisma);
+    console.log("Seed Interviews");
 
-  await prisma.$disconnect();
+    await prisma.$disconnect();
+  } catch (e) {
+    console.log(e);
+  }
 }
