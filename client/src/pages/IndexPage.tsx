d5b253bd87b7d00953756a03518a6b7e6f0211f5
wilsonplau <hello@wilsonplau.com>
2021-07-22 08:08:33 +0900

    feat: add code for index

--- client/src/pages/IndexPage.tsx
+++ client/src/pages/IndexPage.tsx
@@ -1,10 +1,24 @@
 import React from "react";
+
+import IndexPageDetail from "../components/IndexPageDetail/IndexPageDetail";
 import Layout from "../containers/Layout";
+import { useAppDispatch, useAppSelector } from "../hooks";
+import { fetchUserByAccessToken } from "../store/authReducer";
+import "../styles/IndexPage.scss";
 
 const IndexPage: React.FC<{}> = () => {
+  const dispatch = useAppDispatch();
+  const isAuthenticated = useAppSelector((state) => state.auth.isAuthenticated);
+
+  const onLogin = (accessToken: string) => {
+    dispatch(fetchUserByAccessToken(accessToken));
+  };
+
   return (
     <Layout>
-      <h1>Index Page</h1>
+      <div className="index">
+        <IndexPageDetail isAuthenticated={isAuthenticated} onLogin={onLogin} />
+      </div>
     </Layout>
   );
 };
