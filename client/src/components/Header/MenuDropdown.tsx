0fa35a7643a1174a1ebbf866a9d2c7f6d9c1a12d
wilsonplau <hello@wilsonplau.com>
2021-07-22 09:52:54 +0900

    feat: add dashboard link to dropdown

--- client/src/components/Header/MenuDropdown.tsx
+++ client/src/components/Header/MenuDropdown.tsx
@@ -8,6 +8,9 @@ interface MenuDropdownProps {
 const MenuDropdown: React.ForwardRefRenderFunction<HTMLDivElement, MenuDropdownProps> = ({ handleLogout }, ref) => {
   return (
     <div className="menuDropdown" ref={ref}>
+      <a href="/dashboard" className="menuDropdown__button">
+        Dashboard
+      </a>
       <a href="/settings" className="menuDropdown__button">
         Settings
       </a>
