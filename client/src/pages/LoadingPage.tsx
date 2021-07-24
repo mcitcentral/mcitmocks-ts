ea2a5f4a8eaebdefa9c06373eba4b814195f8e2d
wilsonplau <hello@wilsonplau.com>
2021-07-25 08:04:06 +0900

    feat: change loading page

--- client/src/pages/LoadingPage.tsx
+++ client/src/pages/LoadingPage.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Layout from "../containers/Layout";
+import "../styles/LoadingPage.scss";
 
 const LoadingPage: React.FC<{}> = () => {
   return (
     <Layout>
-      <h1>Loading...</h1>
+      <div className="loading">
+        <div className="loader">
+          <div />
+          <div />
+          <div />
+          <div />
+        </div>
+      </div>
     </Layout>
   );
 };
