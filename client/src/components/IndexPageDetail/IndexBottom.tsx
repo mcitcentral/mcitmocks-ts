85046fc1f044edd26a8b86320417224b4b865b7a
wilsonplau <hello@wilsonplau.com>
2021-07-29 20:22:29 +0900

    fix: fix formatting

--- client/src/components/IndexPageDetail/IndexBottom.tsx
+++ client/src/components/IndexPageDetail/IndexBottom.tsx
@@ -2,18 +2,15 @@ import IndexFeature from "./IndexFeature";
 import VideoImage from "../../assets/images/video.svg";
 import ScheduleImage from "../../assets/images/schedule.svg";
 import AdjustImage from "../../assets/images/adjust.svg";
+
 const IndexBottom: React.FC = () => {
-  const titles =
-    ["Set Preferences",
-     "Mark Availability", 
-     "Mock Interview"];
-  const introductions =
-    ["After loggin in, you can set up interview preferences like programming languages, difficullty of questions etc.",
-     "Mark your availabilities on dashbord page,\
-     send interview invitations to matched peers or \
-     accept invitations from others.", 
-     "Click the link on top of your dashbord to access the confirmed interview. \
-     Interact with peers by code editor and two live video screens."];
+  const titles = ["Set Preferences", "Mark Availability", "Mock Interview"];
+  const introductions = [
+    "After logging in, you can set up interview preferences like programming languages, difficullty of questions etc.",
+    "Mark your availabilities on dashbord page, send interview invitations to matched peers or accept invitations from others.",
+    "Click the link on top of your dashbord to access the confirmed interview. Interact with peers by code editor and two live video screens.",
+  ];
+
   return (
     <div className="bottom">
       <div className="bottom__header">How does it work?</div>
