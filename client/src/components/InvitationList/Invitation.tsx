da48ae9fc8040564ddb73e5cb48fe284bf8913de
wilsonplau <hello@wilsonplau.com>
2021-09-24 17:58:37 +0900

    fix: timezone support in invitation list

--- client/src/components/InvitationList/Invitation.tsx
+++ client/src/components/InvitationList/Invitation.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { format } from "date-fns";
+import { format } from "date-fns-tz";
 import { MdCheck, MdClose } from "react-icons/md";
 
 import { InterviewWithUserInfo } from "../../../../@types";
@@ -13,13 +13,14 @@ export interface invitationProps {
 
 const Invitation: React.FC<invitationProps> = ({ interview, handleConfirmInterview, handleRejectInterview }) => {
   const name = interview.inviter.name;
-  const date = format(interview.startTime, "MMMM d 'at' h:mma");
+  const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  const formattedTime = format(interview.startTime, "MMM d 'at' p", { timeZone });
 
   return (
     <div className="invitation">
       <div className="invitation__left">
         <div className="invitation__name">{name}</div>
-        <div className="invitation__time">{date}</div>
+        <div className="invitation__time">{formattedTime}</div>
       </div>
       <div className="invitation__right">
         <button className="invitation__button" onClick={() => handleRejectInterview(interview.id)}>
